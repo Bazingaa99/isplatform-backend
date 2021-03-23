d29a58a04643dc4518ddf1b243692c3f92de2df0
Rokas Vilkaitis <rokas.vilkaitis@mif.stud.vu.lt>
2021-03-23 22:42:32 +0200

    update database on app launch, change items endpoints

--- src/main/java/com/win/itemsharingplatform/controller/ItemController.java
+++ src/main/java/com/win/itemsharingplatform/controller/ItemController.java
@@ -9,32 +9,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/isp")
+@RequestMapping("/isp/items")
 public class ItemController {
     private final ItemService itemService;
 
     public ItemController(ItemService itemService) { this.itemService = itemService; }
 
-    @GetMapping("/all/items")
+    @GetMapping("/all/")
     public ResponseEntity<List<Item>> getAllItems () {
         List<Item> items = itemService.findAllItems();
         return new ResponseEntity<>(items, HttpStatus.OK);
     }
 
-    @GetMapping("/find/item/{id}")
+    @GetMapping("/find//{id}")
     public ResponseEntity<Item> getItemById (@PathVariable("id") Long id) {
         Item item  = itemService.findItemById(id);
         return new ResponseEntity<>(item, HttpStatus.OK);
     }
 
-    @PostMapping("/add/item")
+    @PostMapping("/add/")
     public ResponseEntity<Item> addItem(@RequestBody Item item) {
-        //User owner = userService.findUserById(
         Item newItem = itemService.addItem(item);
         return new ResponseEntity<>(newItem, HttpStatus.CREATED);
     }
 
-    @GetMapping("/find/group/items/{group_id}")
+    @GetMapping("/find/group/{group_id}")
     public ResponseEntity<List<Item>> getItemByGroupId (@PathVariable("group_id") Long groupId) {
         List<Item> item  = itemService.findItemsByGroupId(groupId);
         return new ResponseEntity<>(item, HttpStatus.OK);
