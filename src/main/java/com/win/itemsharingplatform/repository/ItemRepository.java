8dd18725f7d4dfa90e5217a8af6067522722c7a1
Rokas Vilkaitis <rokas.vilkaitis@mif.stud.vu.lt>
2021-04-30 01:50:13 +0300

    fixed: query returns item.isHidden as true even if it is saved as false.

--- src/main/java/com/win/itemsharingplatform/repository/ItemRepository.java
+++ src/main/java/com/win/itemsharingplatform/repository/ItemRepository.java
@@ -10,9 +10,9 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findItemById(Long id);
 
-    @Query(value = "SELECT DISTINCT item.* " +
+    @Query(value = "SELECT * FROM item WHERE item.id = (SELECT DISTINCT item.id " +
                     "FROM item left join request on item.id = request.item_id " +
-                    "WHERE item.group_id = :groupId AND ((request.responded = false OR (request.responded = true AND request.accepted = false)) OR request.item_id is null)", nativeQuery=true)
+                    "WHERE item.group_id = :groupId AND ((request.responded = false OR (request.responded = true AND request.accepted = false)) OR request.item_id is null))", nativeQuery=true)
     List<Item> findItemsByGroupIdAndNotRespondedOrDeclined(Long groupId);
 
     Item findItemByIdAndOwnerId(Long itemId, Long userId);
