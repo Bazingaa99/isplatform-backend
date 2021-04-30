332cb98de361a07e882ac4ee97e9bcdb22aa2fea
Rokas Vilkaitis <rokas.vilkaitis@mif.stud.vu.lt>
2021-05-01 02:51:04 +0300

    Additional checks

---
+++ src/main/java/com/win/itemsharingplatform/exception/RequestDoesNotExistException.java
@@ -0,0 +1,5 @@
+package com.win.itemsharingplatform.exception;
+
+public class RequestDoesNotExistException extends RuntimeException {
+    public RequestDoesNotExistException( String message ){ super(message); }
+}
